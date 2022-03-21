e8e05d23eba3eaef51df56f0abe697a5237e5100
Saoudatou Diarra <saoudatou.diarra@odc.local>
2022-03-21 11:13:16 +0000

    front suite

--- FrontCl/src/app/profil/profil.page.ts
+++ FrontCl/src/app/profil/profil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login/login.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-profil',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.page.scss'],
 })
 export class ProfilPage implements OnInit {
+  client: any;
+  loginData: any;
 
-  constructor() { }
+  constructor(private service : LoginService,public router:Router) { }
 
   ngOnInit() {
+    this.client= JSON.parse(localStorage.getItem('Info'));
+    console.log(this.client.id_client);
+  }
+    
+  logOut(){
+    localStorage.removeItem('Info');
+  this.router.navigate(['/login']);
   }
 
 }
+
+
+
